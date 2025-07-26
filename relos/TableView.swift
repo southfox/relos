f1ca7cf9d3fe8c490081f67418ff2ba4f084f5db
Javier Fuchs <javier.fuchs@gmail.com>
2025-07-26 19:03:50 -0300

    Separating TableView. Adding Snooze and fix isEnabled.

---
+++ relos/TableView.swift
@@ -0,0 +1,18 @@
+//
+//  TableView.swift
+//  relos
+//
+//  Created by Javier Fuchs on 26/07/2025.
+//
+
+import SwiftUI
+
+struct TableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TableView()
+}
